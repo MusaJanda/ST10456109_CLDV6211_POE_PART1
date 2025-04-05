42b71fcac37278af9c90aa227aebcc07000c5d12
MusaJanda <ST10456109@imconnect.edu.za>
2025-04-05 21:47:58 +0200

    Initial commit

---
+++ EventEaseApplication/EventEaseApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EventEaseApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Default route set to HomeController
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Venue", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
